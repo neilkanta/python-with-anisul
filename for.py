f7f05efc5db07d9af49a5cf65e4477197137f768
neilkanta <amitmallick1542000@gmail.com>
2022-09-22 22:21:10 +0600

    first commit

--- for.py
+++ for.py
@@ -1,33 +1,33 @@
-'''num = [10, 20, 50, 40, 60]
+num = [10, 20, 50, 40, 60]
 sum = 0
 for x in num:
     sum = sum+x
     print(x)
-print(sum)'''
+print(sum)
 
-'''list = ['appale', 'mango', 'bal', 'cherry']
+list = ['appale', 'mango', 'bal', 'cherry']
 for x in list:
     print(x)
-print(len(x))'''
+print(len(x))
 
 # break statement we can stop the loop before it has loop through
 
-'''list = [2, 6, 8, 90]
+list = [2, 6, 8, 90]
 for x in list:
     print(x)
     if x == 8:
-        break'''
+        break
 
 # break comes before the print:
 
-'''list = [56, 76, 86, 97]
+list = [56, 76, 86, 97]
 for x in list:
     if x == 86:
         break
-    print(x)'''
+    print(x)
 
-'''list = [20, 40, 60, 80, 65]
+list = [20, 40, 60, 80, 65]
 for x in list:
     if x == 80:
         print(x)
-        continue'''
+        continue
